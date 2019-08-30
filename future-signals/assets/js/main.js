ee48d8717cea9a8056d93eca824296f893893cd0
Gabby Parish <gabrielleparish@hotmail.com>
2019-08-30 11:36:32 +1000

    upload fresh files

---
+++ future-signals/assets/js/main.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+	console.log("The page is ready!");
+
+	// Toggle large research item
+
+	$('.research-item').click(function(){
+		$(this).toggleClass('large')
+	});
+
+	//toggle active class on nav item
+
+	$('.nav-item').click(function(){
+		$('.nav-item').removeClass('active')
+		$(this).addClass('active');
+	});
+
+	var hideAll = function() {
+		$('.research-item') .addClass('hide');
+	}
+
+	//toggle filtered items
+
+	$('.btn-camera').click(function(){
+		hideAll(); 
+		$('.camera').removeClass('hide');
+	});
+
+	$('.btn-luminol').click(function(){
+		hideAll();
+		$('.luminol').removeClass('hide')
+	});
+
+	$('.btn-dna').click(function(){
+		hideAll();
+		$('.dna').removeClass('hide')
+	});
+
+	$('.btn-forarc').click(function(){
+		hideAll();
+		$('.forarc').removeClass('hide')
+	});
+
+	$('.btn-history').click(function(){
+		hideAll();
+		$('.history').removeClass('hide')
+	});
+
+	$('.btn-metadata').click(function(){
+		hideAll();
+		$('.metadata').removeClass('hide')
+	});
+
+	$('.btn-all').click(function(){
+		$('.research-item').removeClass('hide');
+	});
+
+
+});
---
+++ sem1/assets/js/main.js
@@ -0,0 +1,27 @@
+$( document ).ready(function() {
+  Marquee3k.init();
+});
+
+
+$( document ).ready(function() {
+// start here    
+$('').click(function() {
+  $('.issue1').addClass('bottom');
+});
+
+$('.close-link').click(function() {
+  $('.issue1').removeClass('bottom');
+});
+	
+// popup link 
+
+$('.popup-link').click(function() {
+  $('.popup').addClass('active');
+});
+
+$('.popup').click(function() {
+  $('.popup').removeClass('active');
+});
+
+// end here
+});
