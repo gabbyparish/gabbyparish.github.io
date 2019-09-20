7c2c0b3ec7fd8a6f405d12e2fc454aadecb4939d
Gabby Parish <gabrielleparish@hotmail.com>
2019-09-20 11:15:27 +1000

    update

---
+++ geo-graphics/assets/js/main.js
@@ -0,0 +1,39 @@
+ 
+
+ $('.r-happy').mouseenter(function() {
+    $('.p-happy').addClass('p-show');
+  });
+
+$('.r-happy').mouseleave(function() {
+    $('.p-happy').removeClass('p-show');
+  });
+
+ $('.r-sad').mouseenter(function() {
+    $('.p-sad').addClass('p-show');
+  });
+
+$('.r-sad').mouseleave(function() {
+    $('.p-sad').removeClass('p-show');
+  });
+
+
+ $('.r-scared').mouseenter(function() {
+    $('.p-scared').addClass('p-show');
+  });
+
+$('.r-scared').mouseleave(function() {
+    $('.p-scared').removeClass('p-show');
+  });
+
+
+ $('.r-confused').mouseenter(function() {
+    $('.p-confused').addClass('p-show');
+  });
+
+$('.r-confused').mouseleave(function() {
+    $('.p-confused').removeClass('p-show');
+  });
+
+
+
+
