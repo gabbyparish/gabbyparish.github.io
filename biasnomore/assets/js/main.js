c0ad8236f79bb8d2742c1fc4824e14dfd0aca015
Gabby Parish <gabrielleparish@hotmail.com>
2019-10-01 12:25:47 +1000

    update

---
+++ biasnomore/assets/js/main.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+// start here    
+	function on() {
+	  document.getElementById("opening").style.display = "block";
+	}
+
+	function off() {
+	  document.getElementById("opening").style.display = "none";
+	}
+
+}
