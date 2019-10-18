0843d51d8c8849178a5714f080821f29a3add6e6
Gabby Parish <gabrielleparish@hotmail.com>
2019-10-18 12:12:59 +1100

    update

---
+++ lan/assets/js/main.js
@@ -0,0 +1,18 @@
+
+
+jQuery(document).ready(function($) {
+  $('.marquee-text').slick({
+    speed: 5000,
+    autoplay: true,
+    autoplaySpeed: 0,
+    centerMode: true,
+    cssEase: 'linear',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    infinite: true,
+    initialSlide: 1,
+    arrows: false,
+    buttons: false
+  });
+});
