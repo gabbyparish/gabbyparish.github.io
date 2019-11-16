4609f50671e93693364d33fa7e9178d353e8359b
Gabby Parish <gabrielleparish@hotmail.com>
2019-11-16 23:06:07 +1100

    update

--- biasnomore/assets/js/main.js
+++ biasnomore/assets/js/main.js
@@ -1,3 +1,56 @@
+
+$(document).ready(function(){
+  console.log("The page is ready!");
+
+  //toggle active class on nav item
+
+  $('.nav-item').click(function(){
+    $('.nav-item').removeClass('active')
+    $(this).addClass('active');
+  });
+
+  var hideAll = function() {
+    $('.research-item') .addClass('hide');
+  }
+
+  //toggle filtered items
+
+  $('.btn-web').click(function(){
+    hideAll(); 
+    $('.web').removeClass('hide');
+  });
+
+  $('.btn-posters').click(function(){
+    hideAll();
+    $('.posters').removeClass('hide')
+  });
+
+  $('.btn-id').click(function(){
+    hideAll();
+    $('.id').removeClass('hide')
+  });
+
+  $('.btn-print').click(function(){
+    hideAll();
+    $('.print').removeClass('hide')
+  });
+
+  $('.btn-illo').click(function(){
+    hideAll();
+    $('.illo').removeClass('hide')
+  });
+
+  $('.btn-pub').click(function(){
+    hideAll();
+    $('.pub').removeClass('hide')
+  });
+
+  $('.btn-all').click(function(){
+    $('.research-item').removeClass('hide');
+  });
+
+  
+
 // POPUPS
 
   // popup link 
@@ -54,6 +107,5 @@ $('.boxes').click(function() {
 
 });
 
-
-
+});
 
