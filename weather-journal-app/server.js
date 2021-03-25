a108e832e2768ae9887235b37160909b9097c5ed
Gabby Parish <gabrielleparish@hotmail.com>
2021-03-25 17:57:48 +1100

    update

--- weather-journal-app/server.js
+++ weather-journal-app/server.js
@@ -2,18 +2,42 @@
 projectData = {};
 
 // Require Express to run server and routes
+import express, { static } from 'express';
+import { urlencoded, json } from 'body-parser';
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(urlencoded({ extended: false }));
+app.use(json());
 
 // Cors for cross origin allowance
+import cors from 'cors';
+app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(static('website'));
 
+// POST
+app.post('/add', addInfo)
+function addInfo(req, res) {
+    projectData ['date'] = req.body.date;
+    projectData ['content'] = req.body.content;
+    projectData ['temp'] = req.body.temp;
+    req.send('projectData');
+}
+
+// GET
+app.get('/all', getInfo);
+function getInfo(req, res) {
+    res.send('projectData');
+}
 
 // Setup Server
+const port = 3000;
+const server = app.listen(port, listening);
+function listening() {
+    console.log(`running on localhost: ${port}`);
+}
--- weather-journal-app/website/app.js
+++ weather-journal-app/website/app.js
@@ -1,5 +1,94 @@
 /* Global Variables */
+// Personal API Key for OpenWeatherMap API
+let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+let apiKey= 'c637d494b250092c8f1a9aca92a9a5da';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+const userInfo = document.getElementById('entryHolder');
+
+// Event listener to add function to existing HTML DOM element
+const generateButton = document.getElementById("generate");
+generateButton.addEventListener('click', performAction);
+
+/* Function called by event listener */
+function performAction(e) {
+    e.preventDefault();
+    // get user input values
+    const newZip = document.getElementById('zip').value;
+    const content = document.getElementById('feelings').value;
+  
+    getWeather(baseURL, newZip, apiKey)
+      .then(function (userData) {
+        // add data to POST request
+        postData('/add', { date: newDate, temp: userData.main.temp, content })
+      }).then(function (newData) {
+        // call updateUI to update browser content
+        updateUI()
+      })
+    // reset form
+    form.reset();
+  }
+
+/* Function to GET Web API Data*/
+const getWeather = async (baseURL, zipCode, apiKey) => {
+    const res = await fetch(baseURL+zipCode+apiKey);
+    try{
+        const data = await res.json();
+        return data;
+    } catch(error){
+        console.log('error', error);
+    }
+}
+
+/* Function to POST data */
+const postData = async (url='', data={}) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application.json',
+        },
+        body: JSON.stringify({
+            temp: data.temp,
+            date: data.date,
+            content: data.content
+        })
+    });
+
+    try {
+        const newData = await req.json();
+        return newData;
+    } catch (error) {
+        console.log('error', error);
+    }
+};
+
+/* Function to GET Project Data */
+
+const updateUI = async() => {
+    const request = await fetch('/all');
+    try {
+        const allData = await request.json();
+        console.log(allData);
+        if(allData.date !== undefined && allData.temp !== undefined && allData.content !== undefined) {
+            document.getElementById('date').innerHTML = allData.date;
+            document.getElementById('temp').innerHTML = allData.temp;
+            document.getElementById('content').innerHTML = allData.content;
+        }
+    } catch (error) {
+        console.log('error', error);
+    }
+};
+
+function kelvinToCelsius(kelvin) {
+    if (kelvin < (0)) {
+        return 'below absolute zero (0 K)';
+    } else {
+        return (kelvin - 273.15).toFixed(2);
+    }
+}
+
+
