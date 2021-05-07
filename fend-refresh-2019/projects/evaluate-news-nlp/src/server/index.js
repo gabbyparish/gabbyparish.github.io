c36ef6b67bb1c42f79670f927a8abb42f646f39a
Gabby Parish <gabrielleparish@hotmail.com>
2021-05-07 16:14:07 +1000

    new

--- fend-refresh-2019/projects/evaluate-news-nlp/src/server/index.js
+++ fend-refresh-2019/projects/evaluate-news-nlp/src/server/index.js
@@ -1,7 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config();
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
-
+var textapi =new meaningcloud({
+    application_key: "process.env.API_KEY"
+  });
+console.log(`Your API key is ${process.env.API_KEY}`);
 const app = express()
 
 app.use(express.static('dist'))
@@ -14,8 +19,8 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
