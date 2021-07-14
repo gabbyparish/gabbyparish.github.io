e04fab8b445a527861cbcb58cafaf76522267002
Gabby Parish <gabrielleparish@hotmail.com>
2021-07-14 13:41:03 +1000

    update

---
+++ orlando-portfolio/assets/js/glider.js
@@ -0,0 +1,29 @@
+
+// new Glider(document.querySelector('.all-clients'), {
+//     slidesToScroll: 1,
+//     slidesToShow: 5.5,
+//     draggable: true,
+//     dots: '.dots',
+//     arrows: {
+//       prev: '.glider-prev',
+//       next: '.glider-next'
+//     }
+//   });
+
+// const gra = function(min, max) {
+//     return Math.random() * (max - min) + min;
+// }
+
+// const gri = function(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+// const init = function(){
+//   let items = document.querySelectorAll('#all-clients .client-item');
+//   for (let i = 0; i < items.length; i++){
+//     items[i].style.minWidth = gra(30,60) + 'vw';
+//     items[i].style.background = randomColor({luminosity: 'light'});
+//   }
+//   cssScrollSnapPolyfill()
+// }
+// init();
