20bb00aa46d9ae424ce36cd59b40ffd77ae4ad2c
Gabby Parish <gabrielleparish@hotmail.com>
2021-07-15 10:16:09 +1000

    update

--- orlando-portfolio/assets/js/glider.js
+++ orlando-portfolio/assets/js/glider.js
@@ -1,29 +1,14 @@
 
-// new Glider(document.querySelector('.all-clients'), {
-//     slidesToScroll: 1,
-//     slidesToShow: 5.5,
-//     draggable: true,
-//     dots: '.dots',
-//     arrows: {
-//       prev: '.glider-prev',
-//       next: '.glider-next'
-//     }
-//   });
-
-// const gra = function(min, max) {
-//     return Math.random() * (max - min) + min;
-// }
-
-// const gri = function(min, max) {
-//     return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-
-// const init = function(){
-//   let items = document.querySelectorAll('#all-clients .client-item');
-//   for (let i = 0; i < items.length; i++){
-//     items[i].style.minWidth = gra(30,60) + 'vw';
-//     items[i].style.background = randomColor({luminosity: 'light'});
-//   }
-//   cssScrollSnapPolyfill()
-// }
-// init();
+var swiper = new Swiper(".testimonials", {
+   slidesPerView: 1,
+   spaceBetween: 30,
+   loop: true,
+   pagination: {
+     el: ".swiper-pagination",
+     clickable: true,
+   },
+   navigation: {
+     nextEl: ".swiper-button-next",
+     prevEl: ".swiper-button-prev",
+   },
+ });
